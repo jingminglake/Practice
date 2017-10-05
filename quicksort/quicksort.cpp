4c7ea31b0ad7b9f03b9cc3b1ed763fb13b75c393
jingminglake <yaxionh@143.233.21.198.tigernet.wifi.dyn.clemson.edu>
2017-10-05 16:01:35 -0400

    quicksort modify

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void quickSort(int a[], int start, int end) {
+void quickSort(vector<int>& a, int start, int end) {
   if (start >= end)
     return ;
-  int povit = a[start];
+  int pivot = a[start];
   int last = start;
   for (int i = start + 1; i <= end; i++) {
-    if (a[i] < povit)
+    if (a[i] < pivot)
       swap(a[++last], a[i]);
   }
   swap(a[last], a[start]);
   quickSort(a, start, last - 1);
   quickSort(a, last + 1, end);
 }
-
 int main() {
-  int a[10] = {12, 3, 33, 0, 8, 23, 88, 13, 9, 1};
-  quickSort(a, 0, sizeof(a) / sizeof(a[0]) - 1);
-  for (int i = 0; i < 10; i++)
+  vector<int> a = {12, 3, 33, 0, 8, 23, 88, 13, 9, 1};
+  quickSort(a, 0, a.size() - 1);
+  for (int i = 0; i < a.size(); i++)
     cout << a[i] << " ";
   cout << endl;
   return 0;
