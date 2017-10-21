7d4a8e04963029dae517e2731f97a7505a226c1e
jingminglake <yaxionh@144.233.21.198.tigernet.wifi.dyn.clemson.edu>
2017-10-21 14:56:13 -0400

    my c++ split

---
+++ split/split.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+vector<string> split(const string &str, char delim) {
+  istringstream iss(str);
+  vector<string> tokens;
+  string token;
+  while (getline(iss, token, delim)) {
+    tokens.push_back(token);
+  }
+  return tokens;
+}
+
+int main() {
+  string str = "1,2,3,4,5,6,7,8,9";
+  vector<string> vec = split(str, ',');
+  for (string &s : vec)
+    cout << s << " ";
+  cout << endl;
+  return 0;
+}
